257b2cbcccf75aea7b80a2466d58f3a549d3f794
Alain-TC <alain.terieur@hotmail.fr>
2019-10-17 20:49:03 +0200

    refacto pipeline and gridsearch

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from preprocessing.split_dataframe import split_dataframe_by_row
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
 from preprocessing.transformers.fillna_transformer import FillnaMeanTransformer
+from preprocessing.transformers.normalize_transformer import NormalizeTransformer
+from modelisation.model import FullModelClass
 import warnings
 from preprocessing.transformers.standardize_transformer import StandardizeTransformer
 from preprocessing.transformers.onehot_encoder_transformer import SimpleOneHotEncoder
@@ -24,20 +26,19 @@ if __name__ == '__main__':
 
 
     # Transformation log(target)
-
     df_train = transform_log(df_train, 'SalePrice')
 
     # split Train/Eval
     df_train, df_train_eval = split_dataframe_by_row(df_train, 0.7)
 
     # Preprocess data
-
     quantitative_columns =["MSSubClass","LotFrontage","OverallQual","OverallCond","YearBuilt","YearRemodAdd",
                            "MasVnrArea","BsmtFinSF2","BsmtUnfSF","TotalBsmtSF","1stFlrSF","2ndFlrSF","LowQualFinSF",
                            "GrLivArea","BsmtFullBath","BsmtHalfBath","FullBath","HalfBath","BedroomAbvGr",
                            "KitchenAbvGr","TotRmsAbvGrd","Fireplaces","GarageYrBlt","GarageCars","GarageArea",
                             "WoodDeckSF","OpenPorchSF","EnclosedPorch","3SsnPorch","ScreenPorch","PoolArea","MiscVal",
                            "MoSold","YrSold","LotArea"]
+
 
     qualitative_columns = ['Id', 'MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour','Utilities', 'LotConfig',
                            'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle',
@@ -54,65 +55,46 @@ if __name__ == '__main__':
                                         StandardizeTransformer(quantitative_columns),
                                         DataframeToMatrix())
 
+    # Model
+    model = RandomForestRegressor()
+
+    # Pipeline + Model
+    full_model = FullModelClass(processing_pipeline, model)
 
     ###### Entrainement et grid_search
     # Split features and target
     X = df_train.drop(columns='SalePrice')
     y = df_train[['SalePrice']]
 
-    processing_pipeline.fit(X, y)
-    X = processing_pipeline.transform(X)
-
-    # Train Model
-    parameters = {'max_depth': [2*(1+x) for x in range(5)], 'n_estimators': [100, 500, 1000, 1500]}
-    regr = RandomForestRegressor()
-    clf = GridSearchCV(regr, parameters, cv=3)
-    clf.fit(X, y)
-
-    best_params = clf.get_params()
-    print("best_params: {}".format(best_params))
+    parameters = {'model__max_depth': [2 * (1 + x) for x in range(5)], 'model__n_estimators': [100, 500, 1000, 1500]}
+    full_model.fit_grid_search(features=X, target=y, parameters=parameters)
 
     ###### Evaluate Model
     X = df_train_eval.drop(columns='SalePrice')
     y = df_train_eval[['SalePrice']]
 
-    X = processing_pipeline.transform(X)
-
-    y_pred = clf.predict(X)
-
-    error = mean_squared_error(y, y_pred)
+    y_pred = full_model.predict(X)
 
 
     # The mean squared error
+    error = mean_squared_error(y, y_pred)
     print("Mean squared error: %.6f" % error)
     print("Root Mean squared error: %.6f" % np.sqrt(error))
-    # Explained variance score: 1 is perfect prediction
-    print('Variance score: %.6f' % r2_score(y, y_pred))
 
 
-    ###### PREDICTION
-    max_depth = best_params['estimator__max_depth']
-    n_estimators = best_params['estimator__n_estimators']
-    regr = RandomForestRegressor(max_depth=max_depth, random_state=0, n_estimators=n_estimators)
-
+    ###### PREDICTION KAGGLE
+    # Final Train
     final_df_train = pd.concat([df_train, df_train_eval])
-
     X = final_df_train.drop(columns='SalePrice')
     y = final_df_train[['SalePrice']]
+    full_model.fit_model_pipe(X, y)
 
-    processing_pipeline.fit(X, y)
-    X = processing_pipeline.transform(X)
+    # Prediction
+    X_test = pd.read_csv("{}/data/test.csv".format(dir_path))
+    y_pred = full_model.predict(X_test)
 
-    regr.fit(X, y)
-
-    df_test = pd.read_csv("{}/data/test.csv".format(dir_path))
-
-    X_test = df_test
-
-    X_test = processing_pipeline.transform(X_test)
-    y_pred = regr.predict(X_test)
-
-    submission = df_test[['Id']]
+    # Submission
+    submission = X_test[['Id']]
     submission.insert(1, "SalePrice", y_pred, True)
     submission = transform_exp(submission, 'SalePrice')
     submission.to_csv("{}/data/submission.csv".format(dir_path), index=False)
