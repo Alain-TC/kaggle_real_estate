eca7a41f6f6c9c8ec23e150ab3b8bdd2a12f4740
jae <ljs2112@gmail.com>
2019-10-19 00:19:45 +0200

    correction du test_standardize

--- test/test_standardize_transformer.py
+++ test/test_standardize_transformer.py
@@ -7,7 +7,7 @@ import numpy as np
 class TestStandardizeTransformer(unittest.TestCase):
     def setUp(self):
         column_c1 = np.array([0, 2, 1, 0, 2, 2, 0, 2, 0])
-        column_c2 = np.array([0, 1, 3, 1, 2, 4, 4, 1, 2])
+        column_c2 = np.array([0, 0, 3, 1, 2, 4, 4, 1, 2])
         column_c3 = np.array([0, 1, 0, 5, 1, 0, 1, 0, 1])
         column_c4 = np.array([0, 1, 0, 0, 0, 0, 1, 0, np.nan])
         self.df = pd.DataFrame({
@@ -21,8 +21,8 @@ class TestStandardizeTransformer(unittest.TestCase):
 
 
         column_c1 = np.array([-1, 1, 0, -1.0, 1, 1, -1, 1, -1])
-        column_c2 = np.array([-1, -0.5, 0.5, -0.5, 0, 1, 1, -0.5, 0])
-        column_c3 = np.array([-0.25, 0, -0.25, 1, 0, -0.25, 0, -0.25, 0])
+        column_c2 = np.array([-1.2292725943057183, -1.2292725943057183, 0.7231015260621872, -0.5784812208497497, 0.07231015260621876, 1.3738928995181559, 1.3738928995181559, -0.5784812208497497, 0.07231015260621876])
+        column_c3 = np.array([-0.6324555320336759, 0.0, -0.6324555320336759, 2.5298221281347035, 0.0, -0.6324555320336759, 0.0, -0.6324555320336759, 0.0])
         column_c4 = np.array([0, 1, 0, 0, 0, 0, 1, 0, np.nan])
         self.filled_df = pd.DataFrame({
             "column_c1": column_c1,
